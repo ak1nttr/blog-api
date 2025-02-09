e612477ca3c063fb943b91f38d5e5c534f36ef78
ak1nttr <nsnr6458@gmail.com>
2025-02-09 23:32:39 +0300

    swagger configuration and web security configuration to test endpoints

---
+++ src/main/java/com/api/blog/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.api.blog.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+}
