48f805e50a6d3fa17a6b035889d89f6d4438fcca
ak1nttr <nsnr6458@gmail.com>
2025-03-20 23:46:12 +0300

    JWT auth error handling is simplified just for now(logging is removed).

--- src/main/java/com/api/blog/controllers/AuthController.java
+++ src/main/java/com/api/blog/controllers/AuthController.java
@@ -3,13 +3,10 @@ package com.api.blog.controllers;
 import com.api.blog.domain.dtos.AuthResponse;
 import com.api.blog.domain.dtos.LoginRequest;
 import com.api.blog.services.AuthenticationService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,26 +26,17 @@ public class AuthController {
     *  any token generated expires in 24 hours
     * */
     @PostMapping
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
 
-        try {
-            UserDetails userDetails = authService.authenticate(request.getEmail(), request.getPassword());
-            String tokenGenerated = authService.generateToken(userDetails);
-            System.out.println("got here!");
-            AuthResponse response = AuthResponse.builder()
-                    .token(tokenGenerated)
-                    .expiresIn(86400)
-                    .build();
-            logger.info("successful authentication for user: {}",request.getEmail());
-            return ResponseEntity.ok(response);
-        }catch (BadCredentialsException bc) {
-            logger.warn("failed to authenticate for user: {}",request.getEmail());
-            logger.error("error message: {}", bc.getMessage());
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
-        } catch (Exception e) {
-            logger.error("authentication error: {}",e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
+        UserDetails userDetails = authService.authenticate(request.getEmail(), request.getPassword());
+        String tokenGenerated = authService.generateToken(userDetails);
+        System.out.println("got here!");
+        AuthResponse response = AuthResponse.builder()
+                .token(tokenGenerated)
+                .expiresIn(86400)
+                .build();
+        logger.info("successful authentication for user: {}", request.getEmail());
+        return ResponseEntity.ok(response);
         
     }
 
