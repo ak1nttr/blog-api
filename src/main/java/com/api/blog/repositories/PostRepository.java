a821e303632fb07bbf830507fe62f1e3a1e9973c
ak1nttr <nsnr6458@gmail.com>
2025-03-21 18:05:31 +0300

    all repository interfaces are annotated for better error handling.

--- src/main/java/com/api/blog/repositories/PostRepository.java
+++ src/main/java/com/api/blog/repositories/PostRepository.java
@@ -2,9 +2,11 @@ package com.api.blog.repositories;
 
 import com.api.blog.domain.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
 }
--- src/main/java/com/api/blog/repositories/TagRepository.java
+++ src/main/java/com/api/blog/repositories/TagRepository.java
@@ -2,9 +2,11 @@ package com.api.blog.repositories;
 
 import com.api.blog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
 
 }
