c10a1cf9d69dd930f34cac2a42338ffc2d94a0f5
ak1nttr <nsnr6458@gmail.com>
2025-04-27 22:51:42 +0300

    register validation implementation with regex's and error response display

--- src/main/java/com/api/blog/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/api/blog/controllers/GlobalExceptionHandler.java
@@ -3,17 +3,32 @@ package com.api.blog.controllers;
 
 import com.api.blog.domain.dtos.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    private final View error;
+
+    public GlobalExceptionHandler(View error) {
+        this.error = error;
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
@@ -54,5 +69,27 @@ public class GlobalExceptionHandler {
                 .message(bc.getMessage())
                 .build();
         return new ResponseEntity<>(error,HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleArgumentNotValidException(MethodArgumentNotValidException ex){
+        List<ApiErrorResponse.FieldError> errorList = new ArrayList<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError)error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errorList.add(ApiErrorResponse.FieldError.builder()
+                    .field(fieldName)
+                    .message(errorMessage)
+                    .build());
+        });
+
+        ApiErrorResponse response = ApiErrorResponse.builder()
+                .errors(errorList)
+                .message("Validation failed")
+                .status(HttpStatus.BAD_REQUEST.value())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/api/blog/domain/dtos/RegisterRequest.java
+++ src/main/java/com/api/blog/domain/dtos/RegisterRequest.java
@@ -1,7 +1,8 @@
 package com.api.blog.domain.dtos;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,13 +13,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 public class RegisterRequest {
-    @NotNull(message = "Name is required")
+    @NotBlank(message = "Name is required")
+    @Pattern(
+            regexp = "^[a-zA-Z\\s'-]+$",
+            message = "Name should only contain letters, spaces, apostrophes, or hyphens"
+    )
     private String name;
 
-    @NotNull(message = "Email is required")
+    @NotBlank(message = "Email is required")
     @Email(message = "Email is not valid")
     private String email;
 
-    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).+$",
+            message = "Password must contain at least one uppercase letter, one digit, and one special character"
+    )
     private String password;
 }
